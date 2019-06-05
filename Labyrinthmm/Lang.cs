623093cbbcb89af3809cda4a846d497d7e5e0c45
dontru <dontru@onet.pl>
2019-06-05 05:06:08 +0200

    Add class Lang

---
+++ Labyrinthmm/Lang.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinthmm
+{
+    public class Lang
+    {
+        public enum Token { Wall, Space, A, B, NewLine }
+    }
+}
